e78671d0884db965aff7f4be08915684548db540
Chinhtre <77917040+Chinhtre@users.noreply.github.com>
2024-03-05 03:32:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
 global.owner = [
-['524531287294', 'CuriosityBot-MD 🍧', true], 
-['5492266466080', 'Owner 🍧', true], 
-['5217294888993'], 
+['5493855062309', '𝓜𝓲𝓵𝓪𝓬𝓱𝓲𝓷 🎀', true], 
+['5493855062309', 'Owner 🍧', true], 
+['5492995868667'], '𝓜𝓲𝓵𝓪', true],
 ['00'], 
-['5214531287294'], 
-['5492964650915'], 
-['573013482814'], 
-['5492266613038'], 
-['5215610314499'], 
-['51902247905'], 
-['51935531943']]
+['00'], 
+['00'], 
+['00'], 
+['00'], 
+['00'], 
+['00'], 
+['00']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
@@ -33,11 +33,11 @@ global.prems = []
 
 //Aqui encuentras los nombres del bot
 global.packname = ''
-global.author = 'CuriosityBot'
-global.wm = '© CuriosityBot-MD'
-global.wm2 = 'Curiosity : Bot'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot'
+global.author = '𝓜𝓲𝓵𝓪𝓬𝓱𝓲𝓷 🎀'
+global.wm = '𝓜𝓲𝓵𝓪𝓬𝓱𝓲𝓷 🎀'
+global.wm2 = '𝓜𝓲𝓵𝓪𝓬𝓱𝓲𝓷  : Bot'
+global.azami = '𝓜𝓲𝓵𝓪 𝔂 𝓒𝓱𝓲𝓷𝔁'
+global.cb = '𝓜𝓲𝓵𝓪𝓬𝓱𝓲𝓷 '
 
 //Las Versiones Y Entre Otros
 global.vs = 'V2'
@@ -45,8 +45,8 @@ global.library = 'Baileys'
 global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '¿Azami ~ Zam?'
-global.devnum = '+591 62714296'
+global.dev = '𝓜𝓲𝓵𝓪 𝔂 𝓒𝓱𝓲𝓷𝔁'
+global.devnum = '+44'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
